9c0748e040a9f415995858e92e7a3758cf96fe04
Magdalena Hosiawa <mhosiawa@gmail.com>
2013-02-12 20:27:41 +0100

    Set up features and spec files

---
+++ spec/controllers/movie_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe MoviesController do
+	describe 'searching movies with the same director' do
+		it 'should call a model method to search for movies'
+		it 'should select a template for rendering'
+		it 'should make the search results available to rendered template'
+	end
+end
